d05e622a1e0f05f757f94b8d7bbbf1fd34a05074
Ilia Tsarev <bkilja@gmail.com>
2023-10-18 09:53:23 +0400

    changes added

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,8 +1,11 @@
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
@@ -14,42 +17,41 @@ public class CardDeliveryTest {
     void setUp() {
         open("http://localhost:9999");
     }
+    int addDays = 7;
 
-    public String date() {
-        LocalDate currentDate = LocalDate.now();
-        return currentDate.plusDays(5).toString();
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
-
-    public int day() {
-        LocalDate currentDate = LocalDate.now();
-        return currentDate.plusDays(7).getDayOfMonth();
-    }
+    String planningDate = generateDate(addDays, "dd.MM.yyyy");
 
     @Test
     public void shouldTest() {
 
         $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").setValue(date());
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Василий Рука-Нога");
         $("[data-test-id=phone] input").setValue("+79779801234");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
-        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text("Встреча успешно забронирована"));
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
 
     }
 
     @Test
     public void shouldTestCalendar() {
-        String day = Integer.toString(day());
+        String day = generateDate(7, "d");
 
         $("[data-test-id='city'] input").setValue("Мо");
         $$("span.menu-item__control").find(exactText("Москва")).click();
-
-        $("[data-test-id=date]").click();
-        if (LocalDate.now().getDayOfMonth() > LocalDate.now().plusDays(7).getDayOfMonth()) {
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("span.icon").click();
+        if (LocalDate.now().getDayOfMonth() > LocalDate.now().plusDays(addDays).getDayOfMonth()) {
+            $("[data-step='1']").click();
             $$("tr.calendar__row td.calendar__day").find(exactText(day)).click();
         } else {
-            $("[data-step='1']").click();
             $$("tr.calendar__row td.calendar__day").find(exactText(day)).click();
         }
 
@@ -57,7 +59,9 @@ public class CardDeliveryTest {
         $("[data-test-id=phone] input").setValue("+79779801234");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
-        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text("Встреча успешно забронирована"));
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
 
     }
 }
